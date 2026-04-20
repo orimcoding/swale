0f35e94ae9cd259ed336d03c7b12a9cd67aa30ce
Ori Marx <orimarx@gmail.com>
2026-04-20 13:18:57 -0400

    temp useAuth

---
+++ src/lib/hooks/useAuth.ts
@@ -0,0 +1,30 @@
+"use client";
+
+import { createClient } from "@/lib/supabase/client";
+import { useEffect, useState } from "react";
+import type { Session } from "@supabase/supabase-js";
+
+export function useAuth() {
+  const [session, setSession] = useState<Session | null>(null);
+  const [loading, setLoading] = useState(true);
+  const supabase = createClient();
+
+  useEffect(() => {
+    // Check active sessions and sets the user
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+      setLoading(false);
+    });
+
+    // Listen for changes on auth state (logged in, signed out, etc.)
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription?.unsubscribe();
+  }, [supabase.auth]);
+
+  return { session, loading };
+}
